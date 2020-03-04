89a88549109827daa9f25e68d8418ef57d36345c
Gregory Giaquinto <ggiaquinto@intersysconsulting.com>
2020-03-03 23:03:34 -0700

    added more code

--- ETL.py
+++ ETL.py
@@ -1,18 +1,22 @@
 import pandas as pd
-import pymongo
-
-
-rawImdbDF, rawRottenDF = LoadCsv()
-
-cleanImdbDF, cleanRottenDF = CleanData(rawImdbDF, rawRottenDF)
-
-
-
-
+from pymongo import MongoClient
 
 def CleanData(rawImdbDF, rawRottenDF):
     imdbTrimDF = rawImdbDF[['title','year', 'imdbRating', 'ratingCount']]
     rottenTrimDF = rawRottenDF[['movie_title','in_theaters_date', 'audience_rating','audience_count']]
+
+    imdbTrimDF.dropna(inplace=True)
+    rottenTrimDF.dropna(inplace=True)
+
+    imdbTrimDF['title'] = imdbTrimDF['title'].str.split('(').str[0]
+    rottenTrimDF['in_theaters_date'] = rottenTrimDF['in_theaters_date'].str[0:4]
+    rottenTrimDF['in_theaters_date'] = rottenTrimDF['in_theaters_date'].astype(int)
+    rottenTrimDF['audience_rating'] = rottenTrimDF['audience_rating'].astype(float)
+    rottenTrimDF['audience_rating'] = rottenTrimDF['audience_rating'].div(10)
+    rottenTrimDF['audience_count'] = rottenTrimDF['audience_count'].astype(int)
+    rottenTrimDF.reset_index(inplace=True)
+    imdbTrimDF.reset_index(inplace=True)
+    return imdbTrimDF, rottenTrimDF
 
 def LoadCsv():
     #build file location string to read data
@@ -20,12 +24,37 @@ def LoadCsv():
     rottenFile = "Resources/Sources/rotten_tomatoes_movies.csv"
 
     #load movie csv to data frames
-    imdbDF = pd.read_csv(imdbFile)
-    rottenDF = pd.read_csv(rottenFile)
+    imdbDF = pd.read_csv(imdbFile, error_bad_lines=False)
+    rottenDF = pd.read_csv(rottenFile, error_bad_lines=False)
     return imdbDF, rottenDF
 
-#Create Connection to Mongo DB and create the DB
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-mydb = client["MovieAnalysis"]
+def MongoDBInit():
+    #Create Connection to Mongo DB and create the DB
+    client = MongoClient(port=27017)
+    db = client.MovieAnalysisDB
+    return db
+
+def MongoCollection(mongoDB):
+    imdbCollection = mongoDB['IMDB']
+    rottenCollection = mongoDB['Rotten']
+    return imdbCollection, rottenCollection
+
+def MongoInsert(imdbCol, rottenCol, cleanImdbDF, cleanRottenDF):
+    imdbDict = cleanImdbDF.to_dict()
+    imdbCol.insert_one(imdbDict)
+
+    rottenDict = cleanRottenDF.to_dict()
+    rottenCol.insert_one(rottenDict)
+
+
+rawImdbDF, rawRottenDF = LoadCsv()
+
+cleanImdbDF, cleanRottenDF = CleanData(rawImdbDF, rawRottenDF)
+print(cleanImdbDF)
+mongoDB = MongoDBInit()
+
+imdbCol, rottenCol = MongoCollection(mongoDB)
+
+MongoInsert(imdbCol, rottenCol, cleanImdbDF, cleanRottenDF)
+
 
